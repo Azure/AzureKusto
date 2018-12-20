f2d7cb75d1261dd23771796ad73700a45d548e6f
hong-revo <hongooi@microsoft.com>
2018-12-21 00:56:53 +1100

    basic ARM methods

--- R/az_kusto.R
+++ R/az_kusto.R
@@ -2,6 +2,45 @@
 az_data_explorer <- R6::R6Class("az_data_explorer", inherit=AzureRMR::az_resource,
 public=list(
 
+    start=function()
+    {
+        self$do_operation("start", http_verb="POST")
+    },
+
+    stop=function()
+    {
+        self$do_operation("stop", http_verb="POST")
+    },
+
+    create_database=function(database, retention_period=3650, cache_period=31)
+    {
+        op <- file.path("databases", database)
+        properties <- list(
+            softDeletePeriodInDays=retention_period,
+            hotCachePeriodInDays=cache_period
+        )
+        body <- list(
+            properties=properties,
+            location=self$location
+        )
+
+        self$do_operation(op, body=body, encode="json", http_verb="PUT")
+    },
+
+    delete_database=function(database, confirm=TRUE)
+    {
+        if(confirm && interactive())
+        {
+            yn <- readline(paste0("Do you really want to delete the Data Explorer database ",
+                database, "? (y/N) "))
+            if(tolower(substr(yn, 1, 1)) != "y")
+                return(invisible(NULL))
+        }
+
+        op <- file.path("databases", database)
+        self$do_operation(op, http_verb="DELETE")
+    },
+
     get_ade_cluster=function(tenant=NULL)
     {
         clus_name <- sub(".kusto.windows.net$", "", httr::parse_url(self$properties$queryUri)$host)
