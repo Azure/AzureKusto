45d427b493cf151370a0a08bd1d96aa0d0cffd80
Alex Kyllo <alex.kyllo@gmail.com>
2019-01-14 19:23:36 -0800

    use more generic/anonymous cluster name in the tests

--- tests/testthat/test-cluster.R
+++ tests/testthat/test-cluster.R
@@ -2,8 +2,14 @@ context("Kusto Cluster Management")
 
 test_that("normalize_cluster handles clusters with location",
 {
-    cluster_name <- normalize_cluster("hongkust2", normalize_location("australiasoutheast"))
-    expect_equal(cluster_name, "hongkust2.australiasoutheast")
+    cluster_name <- normalize_cluster("help", normalize_location("westus"))
+    expect_equal(cluster_name, "help.westus")
+})
+
+test_that("normalize_cluster handles clusters with location inside the cluster name",
+{
+    cluster_name <- normalize_cluster("help.westus")
+    expect_equal(cluster_name, "help.westus")
 })
 
 test_that("normalize_cluster handles clusters without location",
