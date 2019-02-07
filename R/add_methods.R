e253ce16222f9bb4a89035b055116bb4a3c28b26
hong-revo <hongooi@microsoft.com>
2019-02-07 21:58:11 +1100

    add pkgname:: to resgrp methods

--- R/add_methods.R
+++ R/add_methods.R
@@ -128,7 +128,7 @@ add_methods <- function()
     AzureRMR::az_resource_group$set("public", "create_kusto_cluster", overwrite=TRUE,
     function(name, location=self$location, node_size="D14_v2", ..., wait=TRUE)
     {
-        az_kusto$new(self$token, self$subscription, self$name,
+        AzureKusto::az_kusto$new(self$token, self$subscription, self$name,
             type="Microsoft.Kusto/clusters", name=name, location=location,
             sku=list(name=node_size, tier="Standard"), ..., wait=wait)
     })
@@ -137,7 +137,7 @@ add_methods <- function()
     AzureRMR::az_resource_group$set("public", "get_kusto_cluster", overwrite=TRUE,
     function(name)
     {
-        az_kusto$new(self$token, self$subscription, self$name,
+        AzureKusto::az_kusto$new(self$token, self$subscription, self$name,
             type="Microsoft.Kusto/clusters", name=name)
     })
 
